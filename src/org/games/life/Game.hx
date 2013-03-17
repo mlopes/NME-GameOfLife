923ee7a450b8d974e88588c86fc1c528895510a0
Marco Lopes <marco.paulo.lopes@gmail.com>
2013-03-17 16:51:01 +0000

    Add renderer

--- src/org/games/life/Game.hx
+++ src/org/games/life/Game.hx
@@ -1,45 +1,79 @@
 package org.games.life;
 
-import com.eclecticdesignstudio.motion.Actuate;
-import nme.display.StageAlign;
-import nme.display.StageQuality;
-import nme.display.StageScaleMode;
-import nme.events.Event;
-import nme.Lib;
+import nme.Assets;
+import nme.display.Bitmap;
+import nme.display.Sprite;
+import nme.events.TimerEvent;
+import nme.utils.Timer;
+import org.games.life.Life;
 
-class Game
+class Game extends Sprite
 {
+    private var life:Life;
+    private var timer:Timer;
+    private var gridView:Sprite;
 
-	public function new()
-	{
-		#if (flash9 || flash10)
-		haxe.Log.trace = function(v,?pos) { untyped __global__["trace"](pos.className+"#"+pos.methodName+"("+pos.lineNumber+"):",v); }
-		#elseif flash
-		haxe.Log.trace = function(v,?pos) { flash.Lib.trace(pos.className+"#"+pos.methodName+"("+pos.lineNumber+"): "+v); }
-		#end
-	}
+    public function new():Void
+    {
+        super();
+        gridView = new Sprite();
+    }
 
+    public function start():Void
+    {
+        life = new Life();
+        life.updateGrid(getRandomGeneratedGrid());
 
-	public function initialize ():Void {
-		Lib.current.stage.align = StageAlign.TOP_LEFT;
-		Lib.current.stage.scaleMode = StageScaleMode.NO_SCALE;
-		Lib.current.stage.addEventListener (Event.ACTIVATE, stage_onActivate);
-		Lib.current.stage.addEventListener (Event.DEACTIVATE, stage_onDeactivate);
-	}
+        timer = new Timer(500, 0);
+        timer.addEventListener(TimerEvent.TIMER, tick);
+        render(life.getGrid());
+        timer.start();
+    }
 
-	// Event Handlers
-	private function stage_onActivate (event:Event):Void {
-		Actuate.resumeAll ();
-	}
+    private function tick(e:TimerEvent):Void
+    {
+        life.updateGrid(life.evolve());
+        render(life.getGrid());
+    }
 
+    private function render(grid:Array<Array<Int>>):Void
+    {
+        var newGridView:Sprite = new Sprite();
+        newGridView.x = 0;
+        newGridView.y = 0;
 
-	private function stage_onDeactivate (event:Event):Void {
-		Actuate.pauseAll ();
-	}
+        for (yIndex in 0...grid.length) {
+            for (xIndex in 0...grid[yIndex].length) {
+                if(grid[yIndex][xIndex] == 1) {
+                    var filledCell:Sprite = new Sprite();
+                    filledCell.addChild(new Bitmap(Assets.getBitmapData('images/cell.png')));
+                    filledCell.x = xIndex * 16;
+                    filledCell.y = yIndex * 16;
+                    newGridView.addChild(filledCell);
+                }
+            }
+        }
+        if(contains(gridView)) {
+            removeChild(gridView);
+        }
+        gridView = newGridView;
+        addChild(gridView);
+    }
 
-	// Entry point
-	public static function main(): Void
-	{
-		var game:Game = new Game();
-	}
+    private function getRandomGeneratedGrid(gridWidth:Int = 40, gridHeight:Int = 30):Array<Array<Int>>
+    {
+        var randomGrid:Array<Array<Int>> = new Array();
+
+        for (yIndex in 0...gridHeight) {
+            randomGrid[yIndex] = new Array();
+            for (xIndex in 0...gridWidth) {
+                if(Std.random(11) > 5) {
+                    randomGrid[yIndex][xIndex] = 1;
+                } else {
+                    randomGrid[yIndex][xIndex] = 0;
+                }
+            }
+        }
+        return randomGrid;
+    }
 }
---
+++ src/org/games/life/Main.hx
@@ -0,0 +1,55 @@
+package org.games.life;
+
+import com.eclecticdesignstudio.motion.Actuate;
+import nme.display.StageAlign;
+import nme.display.StageQuality;
+import nme.display.StageScaleMode;
+import nme.events.Event;
+import nme.Lib;
+
+import org.games.life.Game;
+
+class Main
+{
+
+	public function new()
+	{
+		#if (flash9 || flash10)
+		haxe.Log.trace = function(v,?pos) { untyped __global__["trace"](pos.className+"#"+pos.methodName+"("+pos.lineNumber+"):",v); }
+		#elseif flash
+		haxe.Log.trace = function(v,?pos) { flash.Lib.trace(pos.className+"#"+pos.methodName+"("+pos.lineNumber+"): "+v); }
+		#end
+	}
+
+
+	public function initialize ():Void {
+		Lib.current.stage.align = StageAlign.TOP_LEFT;
+		Lib.current.stage.scaleMode = StageScaleMode.NO_SCALE;
+		Lib.current.stage.addEventListener (Event.ACTIVATE, stage_onActivate);
+		Lib.current.stage.addEventListener (Event.DEACTIVATE, stage_onDeactivate);
+	}
+
+	// Event Handlers
+	private function stage_onActivate (event:Event):Void {
+		Actuate.resumeAll ();
+	}
+
+
+	private function stage_onDeactivate (event:Event):Void {
+		Actuate.pauseAll ();
+	}
+
+	private function start():Void
+	{
+		var game:Game = new Game();
+		Lib.current.stage.addChild(game);
+		game.start();
+	}
+
+	// Entry point
+	public static function main(): Void
+	{
+		var main:Main = new Main();
+		main.start();
+	}
+}
